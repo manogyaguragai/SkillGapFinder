632630b65b767a9370a338dc70e96ccff4688949
Manogya Guragai <manogyaguragai24@gmail.com>
2024-09-20 16:48:34 +0545

    :zap: formatting changes

--- searchbot/__init__.py
+++ searchbot/__init__.py
@@ -4,10 +4,20 @@ from llama_index.readers.web import SimpleWebPageReader
 from llama_index.core import PromptTemplate
 from llama_index.core.node_parser import SentenceSplitter
 from llama_index.core import VectorStoreIndex
-import streamlit as st
-def web_searcher(filters):
-  user_question=f'hello'
+
+def web_searcher(data_to_send):
+  
+  level = data_to_send['level'].lower()
+  job = data_to_send.get('job')
+  industry = data_to_send.get('industry')
+
+  if level == 'intern':
+      user_question = f'{job or industry} Internships in Nepal'
+  else:
+      user_question = f'{data_to_send["level"]} {job or industry} Jobs in Nepal'  
+  
   print(f'USER QUESTION: \n\n {user_question}')
+  
   tool_spec = DuckDuckGoSearchToolSpec()
 
   if user_question != "None":
@@ -15,16 +25,16 @@ def web_searcher(filters):
   else:
     search_results = "None"
 
-  st.subheader("Search Results Fetched")
-  st.write(search_results)
+  print("Search Results Fetched")
+  print(search_results)
 
   try:
     urls = [item['href'] for item in search_results]
   except:
     urls = []
 
-  st.subheader("URL LIST EXTRACTED")
-  st.write(urls)
+  print("URL LIST EXTRACTED")
+  print(urls)
 
   try:
       # Fetch data from URLs using TrafilaturaWebReader
@@ -34,38 +44,21 @@ def web_searcher(filters):
       valid_documents = [doc for doc in documents if doc.text is not None]
       
       if valid_documents:
-          st.write("WEB CONTENT FETCHED:")
+          print("WEB CONTENT FETCHED:")
           # for doc in valid_documents:
           #     with st.container(border=True):
           #       st.write(doc)
       else:
-          st.write("No valid content was fetched from the provided URLs.")
+          print("No valid content was fetched from the provided URLs.")
 
   except Exception as e:
-      st.write(f"Error fetching data: {e}")
-      
-  custom_prompt = PromptTemplate(
-    """
-      You are tasked with extracting information about jobs from the provided document. 
-      The document contains job descriptions, requirements, salary information, and various other technical details. 
-      Your task is to provide a brief overview of the job description and focus primarily on elaborating the job requirements.
-      It is crucial that you strictly stick to the information present in the document and do not make any assumptions or hallucinate additional details.
-      Present the requirements as clearly and thoroughly as possible without adding any extraneous information.
-      Refined job description
-      """
-      )
+      print(f"Error fetching data: {e}")
 
   node_parser = SentenceSplitter(chunk_size=1024, chunk_overlap=100)
     
   nodes = node_parser.get_nodes_from_documents(valid_documents, show_progress=True)
 
   index = VectorStoreIndex(nodes)
-      
-  # Settings.llm = OpenAI(model="gpt-4o-mini", temperature=0.0)
-
-  # query_engine = index.as_query_engine(
-          # text_qa_template=custom_prompt
-      # )
       
   llm = OpenAI(model="gpt-4o-mini", temperature=0.00, system_prompt=
               """
@@ -81,22 +74,5 @@ def web_searcher(filters):
               )
 
   question = "Give me a detailed list of Job Description, Hiring Comapny, Salary and Job Requirements from all given documents."
-  response = index.as_query_engine(llm=llm).query(question)
-  st.write(response.response)
 
-  # refined_descriptions = []
-  # for doc in valid_documents:
-  #       # st.write(doc)
-  #       response = query_engine.query(f"Extract and refine the job description, company name, salary and requirements from the following document: {doc}")
-  #       st.write(f'RESPONSE')
-  #       st.write(response.response)
-        # refined_descriptions.append({
-        #     'url': doc.metadata['url'],
-        #     'refined_description': response.response
-        # })
-
-  # for job in refined_descriptions:
-  #     with st.container(border=True):
-  #       st.write(f"URL: {job['url']}")
-  #       st.write(f"Refined Description: {job['refined_description']}")
-  #       st.write("-" * 50)
+  return(index.as_query_engine(llm=llm).query(question).response)

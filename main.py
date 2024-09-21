984bfcab2b7d273b67da666053e1decc4c289467
Apil120 <apilisadhikari@gmail.com>
2024-09-21 07:40:30 +0545

    changes

--- main.py
+++ main.py
@@ -1,96 +1,91 @@
 import streamlit as st
-from searchbot import web_searcher
+import os
+st.set_page_config(page_title="SkillGapFinder", layout="wide",page_icon=os.path.join("assets","logo_transparent.png"))
+
 from ragbot import ragbot
-from utils import get_gap
-upload = st.file_uploader("Upload your syllabus", type="pdf")
-data_from_file = ragbot(uploaded_file=upload)
-print(f'{data_from_file=}')
-columns = st.columns(3)
+import tempfile
+from utils import filters
+cols = st.columns(3)
+cols[0].image(os.path.join("assets","logo_transparent.png"),width=200)
+# st.image(os.path.join("assets","logo_transparent.png"))
+# with st.form(key="select_job_options"):
+with st.container(border=1):
+    upload = st.file_uploader("Upload your syllabus", type="pdf")
+    data  =filters()
+    bitton_col = st.columns([13,0.9])
+    if bitton_col[-1].button("Submit"):
+        if upload:
+            with tempfile.NamedTemporaryFile(delete=False) as temp_file:
+                temp_file.write(upload.read())
+                temp_file_path = temp_file.name
 
-with st.form("my_form"):
-    selected_industry = columns[0].selectbox(
-        label="Choose your industry",
-        options=["IT"]
-    )
-    selected_job = columns[1].selectbox(
-        label="Choose your preferred job",
-        options=[None,"Python developer","Backend developer","Frontend developer","Fullstack developer","Data analyst","UI/UX","Q/A","Product Manager"],index=0
-    )
-
-    selected_level = columns[2].selectbox(
-        label="Choose your level preference",
-        options=["Intern","Junior","Mid-Level","Senior"]
-    )
-
-    data_to_send = {
-        "industry": selected_industry,
-        "job": selected_job,
-        "level": selected_level
-    }
-    if st.form_submit_button("Submit"):
-        # wb_results = web_searcher(data_to_send)
-        st.toast("Your response has been submitted")
+            data_from_file = ragbot(path=temp_file_path)
+            with st.expander("**Course Overview**",expanded=True):
+                    # st.write("")
+                st.write(data_from_file.response)
+                # wb_results = web_searcher(data_to_send)
+        st.toast("Your response has been submitted")#We will replace this with function to do other stuff :)
 
 
-web_saerch = """"
-A mid-level data analyst in Nepal typically engages in the following key responsibilities and requirements, reflective of the evolving tech and data landscape within the region:
+# web_saerch = """"
+# A mid-level data analyst in Nepal typically engages in the following key responsibilities and requirements, reflective of the evolving tech and data landscape within the region:
 
-### Key Responsibilities:
-1. Data Collection and Management:
-    - Collecting data from various sources including databases and digital platforms.
-    - Ensuring data quality and integrity through regular audits and updates.
+# ### Key Responsibilities:
+# 1. Data Collection and Management:
+#     - Collecting data from various sources including databases and digital platforms.
+#     - Ensuring data quality and integrity through regular audits and updates.
 
-2. Data Analysis:
-    - Conducting statistical and analytical studies on datasets.
-    - Identifying trends, patterns, and insights that inform business decisions.
+# 2. Data Analysis:
+#     - Conducting statistical and analytical studies on datasets.
+#     - Identifying trends, patterns, and insights that inform business decisions.
 
-3. Reporting and Visualization:
-    - Creating detailed reports and dashboards using tools like Tableau, Power BI, or Excel.
-    - Presenting data findings to stakeholders in a clear and actionable manner.
+# 3. Reporting and Visualization:
+#     - Creating detailed reports and dashboards using tools like Tableau, Power BI, or Excel.
+#     - Presenting data findings to stakeholders in a clear and actionable manner.
 
-4. Collaboration:
-    - Working closely with cross-functional teams, such as IT, marketing, sales, and finance, to understand their data needs and deliver solutions.
-    - Collaborating with senior analysts and data scientists on complex projects.
+# 4. Collaboration:
+#     - Working closely with cross-functional teams, such as IT, marketing, sales, and finance, to understand their data needs and deliver solutions.
+#     - Collaborating with senior analysts and data scientists on complex projects.
 
-5. Database Management:
-    - Managing and querying data from relational databases using SQL.
-    - Maintaining and updating data warehouses as needed.
+# 5. Database Management:
+#     - Managing and querying data from relational databases using SQL.
+#     - Maintaining and updating data warehouses as needed.
 
-### Skills Requirements:
-1. Technical Skills:
-    - Proficiency in statistical software and tools such as R, Python, SAS, or SPSS.
-    - Advanced Excel skills including pivot tables, v-lookups, and macros.
-    - Strong SQL skills for querying and manipulating data in relational databases.
+# ### Skills Requirements:
+# 1. Technical Skills:
+#     - Proficiency in statistical software and tools such as R, Python, SAS, or SPSS.
+#     - Advanced Excel skills including pivot tables, v-lookups, and macros.
+#     - Strong SQL skills for querying and manipulating data in relational databases.
 
-2. Analytical Skills:
-    - Strong analytical and problem-solving abilities.
-    - Ability to interpret complex data sets and convey findings in a coherent manner.
+# 2. Analytical Skills:
+#     - Strong analytical and problem-solving abilities.
+#     - Ability to interpret complex data sets and convey findings in a coherent manner.
 
-3. Communication Skills:
-    - Excellent verbal and written communication skills to present findings and make recommendations.
-    - Ability to translate technical data into understandable insights for non-technical stakeholders.
+# 3. Communication Skills:
+#     - Excellent verbal and written communication skills to present findings and make recommendations.
+#     - Ability to translate technical data into understandable insights for non-technical stakeholders.
 
-4. Attention to Detail:
-    - High attention to detail to ensure accuracy in data analysis and reporting.
+# 4. Attention to Detail:
+#     - High attention to detail to ensure accuracy in data analysis and reporting.
 
-5. Experience:
-    - Typically, 3-5 years of experience in a data analysis role.
-    - Experience with data visualization tools and software.
+# 5. Experience:
+#     - Typically, 3-5 years of experience in a data analysis role.
+#     - Experience with data visualization tools and software.
 
-### Educational Requirements:
-1. Degree:
-    - A bachelor’s degree in data science, statistics, mathematics, computer science, information management, or a related field.
+# ### Educational Requirements:
+# 1. Degree:
+#     - A bachelor’s degree in data science, statistics, mathematics, computer science, information management, or a related field.
 
-2. Certifications (Preferred but not always required):
-    - Certifications in data analytics, such as Google Data Analytics Certification, Microsoft Certified: Data Analyst Associate, or other relevant certifications.
+# 2. Certifications (Preferred but not always required):
+#     - Certifications in data analytics, such as Google Data Analytics Certification, Microsoft Certified: Data Analyst Associate, or other relevant certifications.
 
-### Job Market Insights:
-- The demand for data analysts in Nepal is growing as more companies realize the importance of data-driven decision-making.
-- Sectors such as telecommunications, finance, healthcare, and e-commerce are prominent employers.
-- Remote work opportunities exist, allowing collaboration with international teams and clients.
+# ### Job Market Insights:
+# - The demand for data analysts in Nepal is growing as more companies realize the importance of data-driven decision-making.
+# - Sectors such as telecommunications, finance, healthcare, and e-commerce are prominent employers.
+# - Remote work opportunities exist, allowing collaboration with international teams and clients.
 
-By combining technical expertise with strong analytical and communication skills, mid-level data analysts in Nepal play a crucial role in helping organizations leverage data for strategic advantages. The evolving tech scene in Nepal offers ample opportunities for growth and professional development in this field.
-"""
-response = get_gap(web_saerch, data_from_file)
+# By combining technical expertise with strong analytical and communication skills, mid-level data analysts in Nepal play a crucial role in helping organizations leverage data for strategic advantages. The evolving tech scene in Nepal offers ample opportunities for growth and professional development in this field.
+# """
+# response = get_gap(web_saerch, data_from_file)
 
-st.write(response)
+# st.write(response)
--- ragbot/__init__.py
+++ ragbot/__init__.py
@@ -1,59 +1,20 @@
-from llama_index.core import SimpleDirectoryReader, VectorStoreIndex
-from llama_index.core.node_parser import SentenceSplitter, SentenceWindowNodeParser
-from llama_index.core.postprocessor import MetadataReplacementPostProcessor
+from llama_index.core import VectorStoreIndex
+from llama_index.readers.file import PyMuPDFReader
 from llama_index.llms.openai import OpenAI
-import os
-import tempfile
 
-def ragbot(uploaded_file):
-  if uploaded_file:
-    query = "Give me detailed information about all the courses, knowledge, skills and programming languages provided in the given document."
-    
-    with tempfile.TemporaryDirectory() as tempdir:
-      uploaded_file_path = os.path.join(tempdir, "uploaded_file.pdf")
-      with open(uploaded_file_path, "wb") as f:
-        f.write(uploaded_file.read())
+loader = PyMuPDFReader()
+LLM = OpenAI(model="gpt-4o-mini",temperature=0.0)
+def ragbot(path):
+  documents = loader.load(file_path=path)
+  query = """
+  Based on the information provided in the document, what technical and non-technical skills can be expected to be learned or gained? 
+  Please also mention any specific tools, languages, or relevant topics referenced in the document.
+  If there are no relevant skills or tools mentioned, please state that there are no relevant skills or tools.
+  """
+  index = VectorStoreIndex.from_documents(documents)
+  query_engine = index.as_query_engine(similarity_top_k=2, llm=LLM)
 
-      reader = SimpleDirectoryReader(input_dir=tempdir)
-      print(reader)
-      
-      documents = reader.load_data()
-      
-      node_parser = SentenceWindowNodeParser.from_defaults(
-          window_size=3,
-          window_metadata_key="window",
-          original_text_metadata_key="original_text",
-      )
-      
-      text_splitter = SentenceSplitter()
+  
+  response = query_engine.query(query)
 
-      # node_parser = SentenceSplitter()
-
-      nodes = node_parser.get_nodes_from_documents(documents, show_progress=True)
-      
-      base_nodes = text_splitter.get_nodes_from_documents(documents)
-      
-      sentence_index = VectorStoreIndex(nodes)
-      
-      base_index = VectorStoreIndex(base_nodes)
-      
-      llm = OpenAI(model="gpt-4o-mini", temperature=0.00,)
-
-      query_engine = sentence_index.as_query_engine(
-            similarity_top_k=2,
-            # the target key defaults to `window` to match the node_parser's default
-            node_postprocessors=[
-                MetadataReplacementPostProcessor(target_metadata_key="window")
-            ],
-            llm=llm
-      )
-      
-      window_response = query_engine.query(query)
-
-      # index = VectorStoreIndex(nodes)
-            
-      # return index.as_query_engine(llm=llm).query(query).response
-      
-      return window_response.response
-
-        
+  return response
